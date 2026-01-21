49e8b970f515b9271e7a4728aa9d69d031b01f31
CODAS <codas0327@gmail.com>
2026-01-21 14:27:01 +0800

    Update background color in Home component for improved visual design

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Dialog from "./components/ui/dialog";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-[#f8f6f2]">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[rgba(0,0,0,0.25)]">
       <Dialog />
     </main>
   );
